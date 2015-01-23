2ff0ea83e8565d7819252ef173723b4b49975fc6
Admin <admin@Mac-Admin.local>
2015-01-23 15:00:56 +0400

    Initial Commit

---
+++ 5ArraysTestHomeWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  5ArraysTestHomeWork
+//
+//  Created by Admin on 23.01.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
